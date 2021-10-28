95d5548465297e9aec90f98f98a0fb2a6239edd3
SlavikChuprin <chuprin.00@yandex.ru>
2021-10-28 21:50:28 +0300

    create hw1-2

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -7,12 +7,14 @@ stats,
 } ) => {
   return (
     <section className={s.statistics}>
+        
   <h2 className={s.title}>Upload stats</h2>
-
+     
   <ul className={s.statList}>
       {stats.map ( stat => {
-          return (<li className={s.item} key={stat.id}>
+          return (<li className={s.item} key={stat.id} style = {{backgroundColor: `#${Math.random().toString(16).substr(2,6)}` }}>
       <span className={s.label}>{stat.label}</span>
+      <bn/>
       <span className={s.percentage}>{stat.percentage}</span>
     </li>)   
       })}
