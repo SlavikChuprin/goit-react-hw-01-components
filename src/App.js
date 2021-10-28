7e6862cd1390f6bea596198d808fe08b83d68e20
SlavikChuprin <chuprin.00@yandex.ru>
2021-10-28 20:22:50 +0300

    create home1-2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Profile from './components/Profile/Profile';
 import user from './components/Profile/user.json'
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './components/Statistics/statistical-data.json';
 import './App.css';
 
 function App() {
@@ -12,9 +14,15 @@ function App() {
          location={user.location}
          avatar={user.avatar}
          stats={user.stats}
-/>
-     
-    </div>
+        />
+
+        <Statistics 
+        title="Upload stats" 
+        stats={statisticalData} 
+        />
+
+     </div>
+
   );
 }
     
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+const Statistics = ({
+title,
+stats,
+} ) => {
+  return (
+    <section className={s.statistics}>
+  <h2 className={s.title}>Upload stats</h2>
+
+  <ul className={s.statList}>
+      {stats.map ( stat => {
+          return (<li className={s.item} key={stat.id}>
+      <span className={s.label}>{stat.label}</span>
+      <span className={s.percentage}>{stat.percentage}</span>
+    </li>)   
+      })}
+  </ul>
+</section>
+  )
+};
+
+Statistics.propTypes = {
+title:PropTypes.string,
+stats:PropTypes.array.isRequired,
+};
+
+export default Statistics;
